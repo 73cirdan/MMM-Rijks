9b93afc22264af015c2b43242fe689167c59ed86
73cirdan <73cirdan@gmail.com>
2020-05-25 17:54:03 +0200

    fault tolerant nodehelper

--- MMM-Rijks.js
+++ MMM-Rijks.js
@@ -15,90 +15,90 @@ Module.register("MMM-Rijks", {
     refreshInterval: 1000*60
   },
   getStyles: function () {
-    return ["MMM-Rijks.css"]
+    return ["MMM-Rijks.css"];
   },
 
   start: function() {
-    this.sendSocketNotification("INIT", this.config)
+    this.sendSocketNotification("INIT", this.config);
   },
 
   getDom: function() {
-    var wrapper = document.createElement("div")
-    wrapper.id = "RIJKS"
-    var image = document.createElement("div")
-    image.id = "RIJKS_IMAGE"
-    var preLoad = document.createElement("div")
-    preLoad.id = "RIJKS_IMAGE_PRELOAD"
-    wrapper.appendChild(image)
-    wrapper.appendChild(preLoad)
+    var wrapper = document.createElement("div");
+    wrapper.id = "RIJKS";
+    var image = document.createElement("div");
+    image.id = "RIJKS_IMAGE";
+    var preLoad = document.createElement("div");
+    preLoad.id = "RIJKS_IMAGE_PRELOAD";
+    wrapper.appendChild(image);
+    wrapper.appendChild(preLoad);
     /* Don't hide copyright. this is defined in terms of usage */
-    var cp = document.createElement("div")
-    cp.id = "RIJKS_COPYRIGHT"
-    cp.innerHTML = "@Rijksmuseum Collection"
-    wrapper.appendChild(cp)
+    var cp = document.createElement("div");
+    cp.id = "RIJKS_COPYRIGHT";
+    cp.innerHTML = "@Rijksmuseum Collection";
+    wrapper.appendChild(cp);
     if (this.config.useDescription) {
-      wrapper.className = "useDescription"
-      var description = document.createElement("div")
-      description.id = "RIJKS_DESCRIPTION"
-      wrapper.appendChild(description)
+      wrapper.className = "useDescription";
+      var description = document.createElement("div");
+      description.id = "RIJKS_DESCRIPTION";
+      wrapper.appendChild(description);
     }
-    return wrapper
+    return wrapper;
   },
 
   socketNotificationReceived: function(notification, payload) {
     switch(notification) {
       case "NEW_IMAGE":
-        this.showImage(payload)
-        break
-      case "SCAN_FAIL":
-        document.getElementById("RIJKS").innerHTML = "Scanning Failed."
-        break
-      case "ITEM_FAIL":
-        document.getElementById("RIJKS").innerHTML = "getting items Failed."
-        break
+        this.showImage(payload);
+        break;
+      //case "SCAN_FAIL":
+        //document.getElementById("RIJKS").innerHTML = "Scanning Failed."
+        //break
+      //case "ITEM_FAIL":
+        //document.getElementById("RIJKS").innerHTML = "getting items Failed."
+        //break
     }
   },
 
   showImage: function(item) {
-    var wrapper = document.getElementById("RIJKS")
-    var image = document.getElementById("RIJKS_IMAGE")
-    var description = document.getElementById("RIJKS_DESCRIPTION")
+    var wrapper = document.getElementById("RIJKS");
+    var image = document.getElementById("RIJKS_IMAGE");
+    var description = document.getElementById("RIJKS_DESCRIPTION");
     if (this.config.useDescription) {
-      description.innerHTML = ""
-      description.style.display= "none"
-      var title = document.createElement("div")
-      title.className = "title"
-      title.innerHTML = item.title
-      description.appendChild(title)
-      var subTitle = document.createElement("div")
-      subTitle.className = "subTitle"
-      subTitle.innerHTML = item.subTitle
-      description.appendChild(subTitle)
-      var scLabelLine = document.createElement("div")
-      scLabelLine.className = "scLabelLine"
-      scLabelLine.innerHTML = item.scLabelLine
-      description.appendChild(scLabelLine)
-      var d = document.createElement("div")
-      d.className = "description"
-      d.innerHTML = item.description
-      description.appendChild(d)
-      var location = document.createElement("div")
-      location.className = "location"
-      location.innerHTML = item.location
-      description.appendChild(location)
+      description.innerHTML = "";
+      description.style.display= "none";
+      var title = document.createElement("div");
+      title.className = "title";
+      title.innerHTML = item.title;
+      description.appendChild(title);
+      var subTitle = document.createElement("div");
+      subTitle.className = "subTitle";
+      subTitle.innerHTML = item.subTitle;
+      description.appendChild(subTitle);
+      var scLabelLine = document.createElement("div");
+      scLabelLine.className = "scLabelLine";
+      scLabelLine.innerHTML = item.scLabelLine;
+      description.appendChild(scLabelLine);
+      var d = document.createElement("div");
+      d.className = "description";
+      d.innerHTML = item.description;
+      description.appendChild(d);
+      var location = document.createElement("div");
+      location.className = "location";
+      location.innerHTML = item.location;
+      description.appendChild(location);
       var timer = setTimeout(()=>{
-        clearTimeout(timer)
-        description.style.display="block"
-      }, 2000)
+        clearTimeout(timer);
+        description.style.display="block";
+      }, 2000);
     }
 
-    var pre = document.getElementById("RIJKS_IMAGE_PRELOAD")
+    var pre = document.getElementById("RIJKS_IMAGE_PRELOAD");
     if (pre.style.backgroundImage) {
-      image.style.backgroundImage = pre.style.backgroundImage
+      image.style.backgroundImage = pre.style.backgroundImage;
     } else {
-      image.style.backgroundImage = `url("${item.cdnUrl}")`
+      image.style.backgroundImage = `url("${item.cdnUrl}")`;
     }
-    pre.style.backgroundImage = `url("${item.nextImage}")`
+    pre.style.backgroundImage = `url("${item.nextImage}")`;
 
   },
 
--- node_helper.js
+++ node_helper.js
@@ -3,114 +3,108 @@
 // This was developed using the Rijksmuseum API.
 //
 
-'use strict'
+'use strict';
 
-const request = require('request')
-
-var NodeHelper = require("node_helper")
+const request = require('request');
+var NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
+
   start: function() {
-    this.collection = []
-    this.index = 0
-    this.timer = null
+    this.collection = [];
+    this.timer = null;
   },
 
   initializeAfterLoading: function (config) {
-    this.config = config
-    this.scanCollection()
+    this.config = config;
+    this.scanCollection();
   },
 
   socketNotificationReceived: function (notification, payload) {
     switch(notification) {
       case 'INIT':
-        this.initializeAfterLoading(payload)
-        this.sendSocketNotification('INITIALIZED')
-        break
+        this.initializeAfterLoading(payload);
+        this.sendSocketNotification('INITIALIZED');
+        break;
     }
   },
 
   scanCollection: function() {
-    var account = this.config.account
-    var query = `https://www.rijksmuseum.nl/api/en/usersets/${account.userId}-${account.setId}?key=${account.apiKey}&format=json`
+    var account = this.config.account;
+    var query = `https://www.rijksmuseum.nl/api/en/usersets/${account.userId}-${account.setId}?key=${account.apiKey}&format=json`;
 
     request(query, { json: true }, (err, res, data) => {
-      if (err) {
-        console.log("[RIJKS] Error:", err)
-        this.sendSocketNotification("SCAN_FAIL")
-        return
+      var error=null;
+      if (err) error = err;
+      else if (!data) error = "statusCode="+ res.statusCode;
+      else if (data.message) error = data.message;
+      else if (!data.userSet) error = "'userSet' is not found. Check your account.";
+      
+      if (error) {
+	console.log("[RIJKS] Collection error: "+error);
+        this.finishScanCollection([]);
+      } else {
+        this.finishScanCollection(data.userSet.setItems);
       }
-      if (data.message) {
-        console.log("[RIJKS] Error:", data.message)
-        this.sendSocketNotification("SCAN_FAIL")
-        return
-      }
-      if (!data.userSet) {
-        console.log("[RIJKS] Error: 'userSet' is not found. Check your account.")
-        this.sendSocketNotification("SCAN_FAIL")
-        return
-      }
-      this.finishScanCollection(data.userSet.setItems)
     })
   },
 
   finishScanCollection: function(items) {
+    this.collection = [];
     for(var i = 0; i < items.length; i++) {
-      var item = items[i]
+      var item = items[i];
       var c = {
         objectNumber: item.objectNumber,
         cdnUrl : item.image.cdnUrl,
         width: item.image.width,
-        height: item.image.height
-      }
-      this.collection.push(c)
+        height: item.image.height,
+        location: "",
+        subTitle: "",
+        scLabelLine: ""
+      };
+      this.collection.push(c);
+      //console.log("[RIJKS] item:"+item.objectNumber);
     }
-    this.index = 0
-    this.startWork()
+    this.startWork(0);
   },
 
-  startWork: function() {
-    clearTimeout(this.timer)
-    this.timer = null
-    var item = this.collection[this.index]
-    this.index++
-    var ti = (this.collection.length <= this.index) ? 0 : this.index
-    var nextImage = this.collection[ti].cdnUrl
-    var account = this.config.account
-    var query = `https://www.rijksmuseum.nl/api/${this.config.descriptionLanguage}/collection/${item.objectNumber}?key=${account.apiKey}&format=json
-`
-    request(query, { json: true }, (err, res, data) => {
-      if (err) {
-        console.log("[RIJKS] Item Error:", err)
-        this.sendSocketNotification("ITEM_FAIL")
-        return
-      } else if (data.message) {
-        console.log("[RIJKS] Item Error:", data.message)
-        this.sendSocketNotification("ITEM_FAIL")
-        return
-      } else if (!data.artObject) {
-        console.log("[RIJKS] Item Error: 'artObject' is not found. There is something wrong.")
-        this.sendSocketNotification("ITEM_FAIL")
-        return
-      } else {
-        item.title = data.artObject.label.title
-        item.description = data.artObject.label.description
-        item.location = data.artObject.location
-        item.subTitle = data.artObject.subTitle
-        item.scLabelLine = data.artObject.scLabelLine
-        item.nextImage = nextImage
-        this.sendSocketNotification("NEW_IMAGE", item)
-        if (this.index >= this.collection.length) {
-          console.log("[RIJKS] 1 cycle finished.")
-          this.scanCollection()
-          return
-        } else {
-          this.timer = setTimeout(()=>{
-            this.startWork()
-          }, this.config.refreshInterval)
+  startWork: function(index) {
+    clearTimeout(this.timer);
+    this.timer = null;
+    if (index < this.collection.length) {
+      var item = this.collection[index];
+      var ni = (this.collection.length == index+1) ? 0 : index+1;
+      item.nextImage = this.collection[ni].cdnUrl;
+
+      var account = this.config.account;
+      var query = `https://www.rijksmuseum.nl/api/${this.config.descriptionLanguage}/collection/${item.objectNumber}?key=${account.apiKey}&format=json`;
+
+      request(query, { json: true }, (err, res, data) => {
+        var error=null;
+        if (err) error = "error messages: " + err; 
+        else if (!data) error = "statuscode: " + res.statusCode;
+        else if (data.message) error = "message: " + data.message;  
+        else if (!data.artObject) error  = "'artObject' is not found. There is something wrong.";
+      
+        if (error) {
+          item.title = item.objectNumber; 
+          item.description = "No description found ("+error+")";
+        }else {
+          item.title = data.artObject.label.title;
+          item.description = data.artObject.label.description;
+          item.location = data.artObject.location;
+          item.subTitle = data.artObject.subTitle;
+          item.scLabelLine = data.artObject.scLabelLine;
         }
-      }
-    })
+        //console.log("[RIJKS] item:"+item.title);
+        this.sendSocketNotification("NEW_IMAGE", item);
+      })
+    } else if (index>0) {
+      //console.log("[RIJKS] 1 cycle finished.");
+      this.scanCollection();
+    }
+    this.timer = setTimeout(()=>{
+      this.startWork(index+1)
+    }, this.config.refreshInterval);
   },
-
 })
